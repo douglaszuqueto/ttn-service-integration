03ccd49b2e009f2c5ef4d428a7dbba4774fbdddc
Douglas Zuqueto <douglas.zuqueto@gmail.com>
2019-07-26 19:36:50 -0300

    [Config] Adição do campo `provider` e `timezone`

--- config/config.go
+++ config/config.go
@@ -1,8 +1,16 @@
 package config
 
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"time"
+)
+
 // Config config
 type Config struct {
 	Provider string `json:"provider"`
+	Timezone string `json:"timezone"`
 	API      struct {
 		Port   int `json:"port"`
 		Secure struct {
@@ -32,4 +40,32 @@ type Config struct {
 			Password string `json:"password"`
 		} `json:"broker"`
 	} `json:"stream"`
+}
+
+// Cfg Cfg
+var Cfg Config
+
+// LoadConfig LoadConfig
+func LoadConfig(configPath string) {
+	// read a config file
+	file, err := os.Open(configPath)
+	if err != nil {
+		log.Fatalln("Arquivo de configuração não encontrado:", err)
+	}
+
+	err = json.NewDecoder(file).Decode(&Cfg)
+	if err != nil {
+		log.Fatalln("Arquivo de configuração não pode ser carregado:", err)
+	}
+
+	// provider
+	if Cfg.Provider != "mqtt" && Cfg.Provider != "http" {
+		log.Fatalln("O serviço de integração possui suporte apenas para o protocolo MQTT ou HTTP!", Cfg.Provider)
+	}
+
+	// test timezone
+	_, err = time.LoadLocation(Cfg.Timezone)
+	if err != nil {
+		log.Fatalln("Timezone inválida:", err)
+	}
 }
